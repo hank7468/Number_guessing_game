f6f63c40823e890d703bd09133732aec577bf7e1
Hank Lee <hank7468@gmail.com>
2019-08-21 11:10:30 +0800

    user defined number scope

--- game.py
+++ game.py
@@ -4,11 +4,16 @@
 # if the answer is correct => print gotcha
 # if the answer is wrong => bigger or smaller
 import random
-r = random.randint(1, 1000)
+x = input('please input the maximum number of the game scope: ')
+x = int(x)
+y = input('please input the minimum number of the game scope: ')
+y = int(y)
+r = random.randint(y, x)
 i = 10 # i = 猜測次數
+print('you have maximun 10 chances to shoot the number.')
 while i > 0:
-	print('you have', i, 'opportunities.')
-	i = i - 1
+	print('chances left: ', i) # 盡量不要把這句寫在if架構裡面，會導致重複性太高
+	i -= 1 # x = x + 1等同於x += 1； x = x - 1等同於x -= 1
 	a = input('please guess the number: ')
 	a = int(a) # r是integer，a也要是integer才能比較
 	if a == r:
