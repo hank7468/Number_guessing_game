92c33a330d2707debaee2f0042fe0f592b6b768f
Hank Lee <hank7468@gmail.com>
2019-08-21 10:27:02 +0800

    my version no.1

---
+++ game.py
@@ -0,0 +1,21 @@
+# number guesing game
+# generate random number by random
+# let user to shoot the number by input
+# if the answer is correct => print gotcha
+# if the answer is wrong => bigger or smaller
+import random
+r = random.randint(1, 1000)
+i = 10 # i = 猜測次數
+while i > 0:
+	i = i - 1
+	a = input('please guess the number: ')
+	a = int(a) # r是integer，a也要是integer才能比較
+	if a == r:
+		print('gotcha!')
+		break
+	elif a > r:
+		print('too large!')
+	else:
+		print('too small!')
+else:
+	print('you are running out oh chance!')
